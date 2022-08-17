0766f7f5455041ee2a65ba05f03241ff369160e6
omrylcn <omeryalcin48@gmail.com>
2022-08-18 01:33:24 +0300

    Edit __init__.py

--- tf_seg/__init__.py
+++ tf_seg/__init__.py
@@ -1,3 +1,3 @@
-from tf_seg.model.resunet import ResUnet
-from tf_seg.model.resunet_pp import ResUnetPlusPlus
-
+from tf_seg.models.resunet import ResUnet
+from tf_seg.models.resunet_pp import ResUnetPlusPlus
+from tf_seg.data import data
