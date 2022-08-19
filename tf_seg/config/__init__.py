0dd3ed8dee46b9b36c58e45721216e0862d427e7
omrylcn <omeryalcin48@gmail.com>
2022-08-19 16:54:17 +0300

    Edit config __init__.py

--- tf_seg/config/__init__.py
+++ tf_seg/config/__init__.py
@@ -1,1 +1,2 @@
-from tf_seg.config.constant import CONFIG_STORE_PATH, CONFIG_FILE_EXTENSION, BASE_CONFIG_LOAD_STYLE
+from tf_seg.config.constant import CONFIG_STORE_PATH, CONFIG_FILE_EXTENSION, CONFIG_LOAD_STYLE_LIB
+from tf_seg.config.config import get_config
