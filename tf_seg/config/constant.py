fc8ac48fa1385f2b384cd84ad601991e06103bd0
omrylcn <omeryalcin48@gmail.com>
2022-08-19 14:48:26 +0300

    Add config constants

---
+++ tf_seg/config/constant.py
@@ -0,0 +1,12 @@
+"""
+
+Constant Parameters, it is not possible change from outside via config file.
+
+"""
+
+# -------------------#
+# config part
+CONFIG_FILE_EXTENSION = ".yaml"
+# constant parameters get from constant.py
+CONFIG_STORE_PATH = "./config"
+BASE_CONFIG_LOAD_STYLE = [".yaml", ".yml", ".py"]
