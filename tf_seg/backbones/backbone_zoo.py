e1e235a1809db7e9651ddca961607bf778335472
omrylcn <omeryalcin48@gmail.com>
2022-08-26 01:01:01 +0300

    Remove unused layer candicates

--- tf_seg/backbones/backbone_zoo.py
+++ tf_seg/backbones/backbone_zoo.py
@@ -6,23 +6,17 @@ https://github.com/yingkaisha/keras-unet-collection/blob/main/keras_unet_collect
 
 from tensorflow.keras.applications import *
 from tensorflow.keras.models import Model
-#from keras_unet_collection.utils import freeze_model
+# from keras_unet_collection.utils import freeze_model
 from tensorflow.python.keras.engine import keras_tensor
 
-import warnings
 
 layer_cadidates = {
-    "VGG16": ("block1_conv2", "block2_conv2", "block3_conv3", "block4_conv3", "block5_conv3"),
-    "VGG19": ("block1_conv2", "block2_conv2", "block3_conv4", "block4_conv4", "block5_conv4"),
     "ResNet50": ("conv1_relu", "conv2_block3_out", "conv3_block4_out", "conv4_block6_out", "conv5_block3_out"),
     "ResNet101": ("conv1_relu", "conv2_block3_out", "conv3_block4_out", "conv4_block23_out", "conv5_block3_out"),
     "ResNet152": ("conv1_relu", "conv2_block3_out", "conv3_block8_out", "conv4_block36_out", "conv5_block3_out"),
     "ResNet50V2": ("conv1_conv", "conv2_block3_1_relu", "conv3_block4_1_relu", "conv4_block6_1_relu", "post_relu"),
     "ResNet101V2": ("conv1_conv", "conv2_block3_1_relu", "conv3_block4_1_relu", "conv4_block23_1_relu", "post_relu"),
     "ResNet152V2": ("conv1_conv", "conv2_block3_1_relu", "conv3_block8_1_relu", "conv4_block36_1_relu", "post_relu"),
-    "DenseNet121": ("conv1/relu", "pool2_conv", "pool3_conv", "pool4_conv", "relu"),
-    "DenseNet169": ("conv1/relu", "pool2_conv", "pool3_conv", "pool4_conv", "relu"),
-    "DenseNet201": ("conv1/relu", "pool2_conv", "pool3_conv", "pool4_conv", "relu"),
     "EfficientNetB0": ("block2a_expand_activation", "block3a_expand_activation", "block4a_expand_activation", "block6a_expand_activation", "top_activation"),
     "EfficientNetB1": ("block2a_expand_activation", "block3a_expand_activation", "block4a_expand_activation", "block6a_expand_activation", "top_activation"),
     "EfficientNetB2": ("block2a_expand_activation", "block3a_expand_activation", "block4a_expand_activation", "block6a_expand_activation", "top_activation"),
@@ -34,8 +28,7 @@ layer_cadidates = {
 }
 
 
-def get_backbone(backbone_name: str, weights: str, input_tensor: keras_tensor.KerasTensor, depth: int,
-                 freeze_backbone: bool = True, freeze_batch_norm: bool = False):
+def get_backbone(backbone_name: str, weights: str, input_tensor: keras_tensor.KerasTensor, depth: int, freeze_backbone: bool = True, freeze_batch_norm: bool = False):
     """
     Configuring a user specified encoder model based on the `tensorflow.keras.applications`
 
