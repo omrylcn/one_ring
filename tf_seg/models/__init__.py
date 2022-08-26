b6ec05777c71f560b874a5dbc61898be93a7682d
omrylcn <omeryalcin48@gmail.com>
2022-08-27 00:09:19 +0300

    Add model builder function

--- tf_seg/models/__init__.py
+++ tf_seg/models/__init__.py
@@ -1,3 +1,26 @@
+from typing import Union
+from tensorflow.keras.models import Model
+from omegaconf import DictConfig, ListConfig
+
 from tf_seg.models.unet import Unet
 from tf_seg.models.resunet import ResUnet
 from tf_seg.models.resunet_pp import ResUnetPlusPlus
+
+model_lib = {"unet": Unet, "resunet": ResUnet, "resunet_pp": ResUnetPlusPlus}
+
+
+def pascal_case_to_snake_case(s:str)->str:
+    """Convert class name to snake case name."""
+    return "".join(["_"+i.lower() if i.isupper() else i for i in s]).lstrip("_")
+
+
+def get_model_builder(config:Union[DictConfig, ListConfig])->Model:
+    """Get model builder  from config file"""
+    class_name = pascal_case_to_snake_case(config.model.class_name)
+    model = model_lib[class_name]
+
+    model_config = config.model.copy()    
+    model_config.pop("class_name")
+    
+    return model(**model_config)
+
