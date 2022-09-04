fe5837323c8d8f57b5ddb52b873c3d50c92761bc
omrylcn <omeryalcin48@gmail.com>
2022-09-04 18:36:55 +0300

    Fİx output_type

--- tf_seg/utils/aug_func.py
+++ tf_seg/utils/aug_func.py
@@ -5,8 +5,9 @@ import albumentations as A
 class AlbumentatiosWrapper:
     """Albumentation Wrapper to use tensorflow dataset."""
 
-    def __init__(self, transforms, output_type=[tf.float32, tf.float32]):
+    def __init__(self, transforms, output_type=[tf.uint8, tf.uint8]):
         self.transforms = transforms
+        self.output_type = output_type
 
     def _aug_albument(self, image, mask):
         data = {"image": image, "mask": mask}
@@ -17,5 +18,5 @@ class AlbumentatiosWrapper:
 
     @tf.function
     def __call__(self, image, mask):
-        aug_img, aug_mask = tf.numpy_function(func=self._aug_albument, inp=[image, mask], Tout=[tf.float32, tf.float32])
+        aug_img, aug_mask = tf.numpy_function(func=self._aug_albument, inp=[image, mask], Tout=self.output_type)
         return aug_img, aug_mask
