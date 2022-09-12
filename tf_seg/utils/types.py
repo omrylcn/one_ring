8d3c0326280ce091333445e6918b31f0521a243a
omrylcn <omeryalcin48@gmail.com>
2022-09-12 16:16:27 +0300

    Edit file

--- tf_seg/utils/types.py
+++ tf_seg/utils/types.py
@@ -1,18 +1,5 @@
-# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""Types for typing functions signatures.
+"""
+Types for typing functions signatures.
 
 # Check more types for :
 - https://github.com/tensorflow/agents/blob/v0.13.0/tf_agents/typing/types.py
@@ -55,9 +42,7 @@ Regularizer = Union[None, dict, str, Callable, tf.keras.regularizers.Regularizer
 Constraint = Union[None, dict, str, Callable, tf.keras.constraints.Constraint]
 Activation = Union[None, str, Callable]
 if importlib.util.find_spec("tensorflow.keras.optimizers.legacy") is not None:
-    Optimizer = Union[
-        tf.keras.optimizers.Optimizer, tf.keras.optimizers.legacy.Optimizer, str
-    ]
+    Optimizer = Union[tf.keras.optimizers.Optimizer, tf.keras.optimizers.legacy.Optimizer, str]
 else:
     Optimizer = Union[tf.keras.optimizers.Optimizer, str]
 
@@ -75,9 +60,4 @@ FloatTensorLike = Union[tf.Tensor, float, np.float16, np.float32, np.float64]
 AcceptableDTypes = Union[tf.DType, np.dtype, type, int, str, None]
 
 
-Tensor = Union[
-    python.framework.ops.Tensor,
-    python.framework.sparse_tensor.SparseTensor,
-    python.ops.ragged.ragged_tensor.RaggedTensor,
-    python.framework.ops.EagerTensor
-]
+Tensor = Union[python.framework.ops.Tensor, python.framework.sparse_tensor.SparseTensor, python.ops.ragged.ragged_tensor.RaggedTensor, python.framework.ops.EagerTensor]
