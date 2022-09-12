a70124fe153ff6ca90d8d47f832750a723bf29de
omrylcn <omeryalcin48@gmail.com>
2022-09-13 01:03:00 +0300

    Add  callback attribute

--- tf_seg/train.py
+++ tf_seg/train.py
@@ -5,9 +5,8 @@ trainer class yapısı
 - objelerin eğitim ve evaluate işlemlerini yapar.
 - 
 """
-
-
-from typing import Union, Optional, List
+import os
+from typing import Union, Optional, List, Dict
 from omegaconf import DictConfig, ListConfig
 from tensorflow.keras import Model
 from tensorflow.data import Dataset
@@ -27,7 +26,7 @@ class Trainer:
         model: Model,
         train_data: Dataset,
         val_data: Optional[Dataset] = None,
-        callbacks: Optional[List[Callback]] = None,
+        callbacks: Optional[Union[Dict[str, Callback], List[Callback]]] = None,
     ) -> None:
         """
 
@@ -50,13 +49,13 @@ class Trainer:
         self._model = model
         self.train_data = train_data
         self.val_data = val_data
-        self.callbacks = callbacks
+        self._callbacks = callbacks
 
         self._check_params()
 
     def _check_params(self) -> None:
         "Check parameters types"
-        assert isinstance(self.config, (DictConfig, ListConfig))
+        assert isinstance(self.config, (dict, DictConfig, ListConfig))
         assert isinstance(self._model, Model)
         assert isinstance(self.train_data, Dataset)
 
@@ -64,31 +63,39 @@ class Trainer:
             assert isinstance(self.val_data, Dataset)
 
         if self.callbacks:
-            assert isinstance(self.callbacks, List)
             if len(self.callbacks) > 0:
                 for callback in self.callbacks:
                     assert isinstance(callback, Callback)
-
+        
     def _check_trainer_params(self):
         "Check trainer config parameters"
+        assert self.config["epochs"] > 0
+        assert self.config["batch_size"] > 0
+        assert self.config["optimizer"] is not None
+        assert self.config["losses"] is not None
 
-        assert self.config.epochs > 0
-        assert self.config.batch_size > 0
-        assert self.config.optimizer is not None
-        assert self.config.losses is not None
+    @property
+    def callbacks(self) -> None:
+        " Convert  list of callbacks "
+        if self._callbacks:
+            assert isinstance(self._callbacks, (List, Dict))
+            if type(self._callbacks) == List:
+                return self._callbacks
+            else:
+                return list(self._callbacks.values())
+
+        return None
 
     @property
     def model(self):
         print("test")
         self._check_trainer_params()
 
-        optimizer_conf = self.config.optimizer
-        optimizer = eval(snake_case_to_pascal_case(optimizer_conf["name"]))(
-            **optimizer_conf["params"]
-        )
+        optimizer_conf = self.config["optimizer"]
+        optimizer = eval(snake_case_to_pascal_case(optimizer_conf["name"]))(**optimizer_conf["params"])
 
-        losses = [eval(snake_case_to_pascal_case(i))() for i in self.config.losses]
-        metrics = [eval(snake_case_to_pascal_case(i))() for i in self.config.metrics]
+        losses = [eval(snake_case_to_pascal_case(i))() for i in self.config["losses"]]
+        metrics = [eval(snake_case_to_pascal_case(i))() for i in self.config["metrics"]]
 
         self._model.compile(optimizer=optimizer, loss=losses, metrics=metrics)
         return self._model
@@ -98,19 +105,21 @@ class Trainer:
 
         self.model.fit(
             self.train_data,
-            epochs=self.config.epochs,
+            epochs=self.config["epochs"],
             callbacks=self.callbacks,
             validation_data=self.val_data,
         )
 
-    def evaluate(self):
+    def evaluate(self) -> None:
         if self.val_data:
             self.model.evaluate(self.val_data)
         else:
             raise ValueError("Validation data is not provided")
 
-    def save(self, filename):
+    def save(self, path: str, filename: str) -> None:
+        assert os.path.exists(path), f"Path: {path} does not exist"
+
         self.model.save(filename)
 
-    def test(self, data):
+    def test(self, data) -> None:
         self.model.evaluate(data)
