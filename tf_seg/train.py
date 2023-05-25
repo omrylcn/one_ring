1c6c634913797757e5a3a2926ab6c0b1c20bb98b
omrylcn <omeryalcin48@gmail.com>
2023-05-25 12:16:37 +0300

    Add save method

--- tf_seg/train.py
+++ tf_seg/train.py
@@ -6,8 +6,11 @@ trainer class yapısı
 - 
 """
 import os
+import uuid
+import datetime
 import pickle
 import tf2onnx
+
 
 from typing import Union, Optional, List, Dict
 from omegaconf import DictConfig, ListConfig
@@ -24,7 +27,7 @@ from tf_seg.metrics import METRICS
 from tf_seg.logger import Logger
 
 # from tf_seg.utils import snake_case_to_pascal_case
-from tf_seg.deploy import export_to_onnx
+from tf_seg.deploy import save_model_as_onnx
 
 
 OPTIMIZERS = {"sgd": SGD, "rmsprop": RMSprop, "adam": Adam, "adadelta": Adadelta, "adagrad": Adagrad, "adamax": Adamax, "nadam": Nadam, "ftrl": Ftrl}
@@ -77,6 +80,8 @@ class Trainer:
         self._check_trainer_objects()  # check objects
         self._check_trainer_params()  # check parameters
 
+        self.history_dict = {}
+
     def _check_trainer_objects(self) -> None:
         "Check parameters types"
         assert isinstance(self.config, (dict, DictConfig, ListConfig))
@@ -87,14 +92,15 @@ class Trainer:
             assert isinstance(self.val_data, Dataset)
 
         if self._callbacks:
-            if isinstance(self._callbacks, Callback):
-                pass
 
-            else:
+            if isinstance(self._callbacks, (List)):
                 assert isinstance(self._callbacks, (List)), "'callbacks' object should be list"
                 if len(self._callbacks) > 0:
                     for callback in self._callbacks:
                         assert isinstance(callback, Callback)
+            else:
+                assert isinstance(self._callbacks, (Callback)), "a callback should be instance of tf.keras.callbacks.Callback"
+
         if self._optimizer:
             assert isinstance(self._optimizer, Optimizer)
 
@@ -143,11 +149,11 @@ class Trainer:
         callbacks = []
 
         if self._callbacks:
-            assert isinstance(self._callbacks, (List, Dict))
-            if type(self._callbacks) == List:
+            assert isinstance(self._callbacks, (List))
+            if type(self._callbacks) == list:
                 return self._callbacks
             else:
-                return list(self._callbacks.values())
+                return list(self._callbacks)
 
         else:
             return callbacks
@@ -176,7 +182,6 @@ class Trainer:
             message = "No loss is specified in config file or in loss object in __init__() method."
             self.log(message, level=0)
             raise ValueError(message)
-
         return losses
 
     @property
@@ -223,78 +228,114 @@ class Trainer:
         return optimizer
 
     def compile_model(self):
+        self.uuid = str(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
         self.loss = self.trainer_loss
         self.metrics = self.trainer_metrics
         self.optimizer = self.trainer_optimizer
+        self.callbacks = self.trainer_callbacks
 
         self._model.compile(optimizer=self.optimizer, loss=self.loss, metrics=self.metrics)
 
         self.log(message="Model is completed", level=2)
 
+    def fit(self, continue_training: bool = True) -> None:
+        """
+        Train tf keras model
 
+        Parameters
+        ----------
+        continue_training : bool
+            If True, continue training from last epoch. If False, recompile model and start training from scratch.
+        """
+
+        if not self._model._is_compiled or not continue_training:
+            self.compile_model()
+
+        self.log("Start training  ...", level=2)
+
+        history = self._model.fit(self.train_data, epochs=self.trainer_config["epochs"], callbacks=self.callbacks, validation_data=self.val_data)
+        self.history_dict[str(datetime.datetime.now())] = {"h": history.history, "id": self.uuid}
+
+        current_time = str(datetime.datetime.now())
+        self.log(f"Training ended at {current_time}. History saved with id {self.uuid}", level=2)
+
+        # if self.trainer_config["save_model"]:
+
+            #path = os.path.join(path,self.uuid) 
+            #os.makedirs(path, exist_ok=True)
+        #     self.save(self.trainer_config["save_model_path"])
     
-        #
-
-    # @property
-    # def model(self):
-    #     self._check_trainer_params()
-
-    #     optimizer_conf = self.config["optimizer"]
-    #     optimizer = eval(snake_case_to_pascal_case(optimizer_conf["name"]))(**optimizer_conf["params"])
-
-    #     losses = [eval(snake_case_to_pascal_case(i))() for i in self.config["losses"]]
-    #     metrics = [eval(snake_case_to_pascal_case(i))() for i in self.config["metrics"]]
-
-    #     self._model.compile(optimizer=optimizer, loss=losses, metrics=metrics)
-    #     return self._model
-
-    def train(self, continue_training: bool = False) -> None:
-        "Train tf keras model"
-
-        # TODO: merge continue training and first histories
-        if continue_training and self._model._is_compiled:
-            self.history = self._model.fit(self.train_data, epochs=self.config["epochs"], callbacks=self.callbacks, validation_data=self.val_data)
-
-        else:
-            self.history = self.model.fit(self.train_data, epochs=self.config["epochs"], callbacks=self.callbacks, validation_data=self.val_data)
-
-        if self.config["save_model"]:
-            # TODO: add logger
-            self.save(path=self.config["save_name"])
+    @property
+    def history(self):
+        return self.history_dict
 
     def evaluate(self) -> None:
         if self.val_data:
             self._model.evaluate(self.val_data)
         else:
+            self.log(message="Validation data is not provided", level=0)
             raise ValueError("Validation data is not provided")
 
     def save(self, path: str, meta_data_name="meta_data.pkl", onnx_name="model.onnx") -> None:
         """
-        Saves the model to Tensorflow SavedModel
+        Saves the model to Tensorflow SavedModel and optionally to ONNX format. Also saves related metadata and preprocessor objects.
 
         Parameters
         ----------
         path: str
-            Path to save model
-        filename: str, default: None
-            Filename to save model
-
+            Directory path to save model and related files.
+        meta_data_name: str, default: "meta_data.pkl"
+            Filename to save metadata.
+        onnx_name: str, default: "model.onnx"
+            Filename to save model in ONNX format.
         """
 
-        self._model.save(path)
-        self._save_meta_data(path, meta_data_name)
+        
 
-        if self.config["deploy_onnx"]:
-            self._export_to_onnx(model_name=path, onnx_name=onnx_name)
+        # check path 
+        #if not os.path.exists(path):
+        os.makedirs(path, exist_ok=True)
+       
+        tf_path = os.path.join(path, "tensorflow")
+        os.makedirs(tf_path, exist_ok=True)
 
-    def _export_to_onnx(self, model_name: str, onnx_name: str, opset: int = 13) -> None:
+        onnx_path = os.path.join(path, "onnx")
+        os.makedirs(onnx_path, exist_ok=True)
+
+        meta_data_path = os.path.join(path, "meta_data")
+        os.makedirs(name=meta_data_path, exist_ok=True)
+
+        processors_path = os.path.join(path, "processors")
+        os.makedirs(name=processors_path, exist_ok=True)
+
+
+        # save model
+        self._model.save(tf_path)
+        self.log(message=f"Model is saved to {path}", level=2)
+
+        # save meta data
+        self._save_meta_data(meta_data_path, meta_data_name)
+        self.log(message=f"Meta data is saved to {meta_data_path}", level=2)
+        
+        # save processors
+        self._save_processors(processors_path, "processors.pkl")
+        self.log(message=f"Processors are saved to {processors_path}", level=2)
+
+        # save onnx
+        if self.trainer_config["deploy_onnx"]:
+            onnx_name = os.path.join(onnx_path, onnx_name)
+            self._save_as_onnx(model=self._model, onnx_name=onnx_name)
+            self.log(message=f"ONNX model is saved to {onnx_path}", level=2)
+
+
+    def _save_as_onnx(self, model, onnx_name: str, opset: int = 13) -> None:
         """
-        Export model to onnx
+        Save model as onnx file
 
         Parameters
         ----------
-        model_name: str
-            Path to model
+        model: tf.keras.models.Model
+            Model to save
         onnx_name: str
             Name of onnx file
         opset: int, default: 13
@@ -302,13 +343,20 @@ class Trainer:
 
         """
 
-        export_to_onnx(model_name, onnx_name, opset=opset)
+        save_model_as_onnx(model, onnx_name, opset=opset)
 
     def _save_meta_data(self, path: str, filename: str) -> None:
         "Save meta data to model path"
         assert os.path.exists(path), f"Path: {path} does not exist"
         with open(path + "/" + filename, "wb") as f:
-            pickle.dump(self.all_config, f)
+            pickle.dump(self.config, f)
+        
+    def _save_processors(self, path: str, filename: str) -> None:
+        "Save proto data to model path"
+        pass
+        # assert os.path.exists(path), f"Path: {path} does not exist"
+        # with open(path + "/" + filename, "wb") as f:
+        #     pickle.dump(self.proto_data, f)
 
     def load(self, path: str, checking_parameters: bool = True) -> None:
         """
@@ -322,30 +370,32 @@ class Trainer:
             Check parameters of model and config file
 
         """
-        try:
-            _model = load_model(path)
-            if checking_parameters:
-                config = self.all_config["model"]
-                print(config)
-                print(config["input_shape"])
 
-                # input shape check config and loaded model
-                assert list(config["input_shape"]) == list(_model.input_shape[1:]), f"Input shape of model is not equal to config input shape {config['input_shape']} != {_model.input_shape[1:]}"
+        pass
+        # try:
+        #     _model = load_model(path)
+        #     if checking_parameters:
+        #         config = self.all_config["model"]
+        #         print(config)
+        #         print(config["input_shape"])
 
-                # output shape check config and loaded model
-                config_output_shape = list(config["input_shape"])
-                config_output_shape[-1] = config["output_size"]
-                assert config_output_shape == list(_model.output_shape[1:])
+        #         # input shape check config and loaded model
+        #         assert list(config["input_shape"]) == list(_model.input_shape[1:]), f"Input shape of model is not equal to config input shape {config['input_shape']} != {_model.input_shape[1:]}"
 
-                # final activation check config and loaded model
-                model_last_activation = _model.layers[-1].activation.__name__
-                config_last_activation = config["final_activation"]
-                assert model_last_activation == config_last_activation, f"Final activation of model is not equal to config final activation {config_last_activation} != {model_last_activation}"
+        #         # output shape check config and loaded model
+        #         config_output_shape = list(config["input_shape"])
+        #         config_output_shape[-1] = config["output_size"]
+        #         assert config_output_shape == list(_model.output_shape[1:])
 
-            self._model = _model
+        #         # final activation check config and loaded model
+        #         model_last_activation = _model.layers[-1].activation.__name__
+        #         config_last_activation = config["final_activation"]
+        #         assert model_last_activation == config_last_activation, f"Final activation of model is not equal to config final activation {config_last_activation} != {model_last_activation}"
 
-        except Exception as e:
-            raise ValueError("Model could not be loaded", e)
+        #     self._model = _model
+
+        # except Exception as e:
+        #     raise ValueError("Model could not be loaded", e)
 
     def test(self, data) -> None:
         self._model.evaluate(data)
