a7815c264940265cc834f4c32d6864111a7e1fe9
yesilyurtihsan <yesilyrtihsan@gmail.com>
2023-09-21 00:10:18 +0300

    Add deepvision segformer model

--- tf_seg/models/segformer.py
+++ tf_seg/models/segformer.py
@@ -1,17 +1,22 @@
-from typing import Tuple
-from transformers import TFSegformerForSemanticSegmentation
+import os
+from typing import Tuple, List, Dict, Any, Union, Optional
 
+import tensorflow as tf
 from tensorflow.keras.models import Model
+
 from tf_seg.base import ModelBuilder
 
 
 class SegFormer(ModelBuilder):
+    """A model builder for SegFormer semantic segmentation model."""
+
     def __init__(
         self,
+        backbone: str,
+        input_shape: Tuple[int],
         output_size: int,
-        input_shape: Tuple = (512, 512, 3),
-        backbone: str = "nvidia/mit-b0",
-    ) -> None:
+        softmax_output: bool = True,
+    ):
         """Initializes the model builder.
         
         Parameters
@@ -26,35 +31,28 @@ class SegFormer(ModelBuilder):
 
         Methods
         -------
-        build_model(self, label_names)
-            Builds and returns the model given the name for the output labels as list.
+        build_model(self)
+            Builds and returns the model.
 
         Notes
         -----
         SegFormer Article: https://arxiv.org/abs/2105.15203
-        
-        Huggingface SegFormer Documentation: https://huggingface.co/docs/transformers/main/model_doc/segformer
-        """
-        self.output_size = output_size
-        self.input_shape = input_shape
-        self.backbone = backbone
 
-    def build_model(self, label_names: list) -> Model:
+
+
+        """
+        self.backbone = backbone
+        self.input_shape = input_shape
+        self.output_size = output_size
+        self.softmax_output = softmax_output
+
+    def build_model():
         """Builds the model.
 
         Returns
         -------
         Model : tf.keras.model.Model
-            The SegFormer model.
-        """
-        id2label = {i: label_names[i] for i in range(self.output_size)}
-        label2id = {label_names[i]: i for i in range(self.output_size)}
-        model = TFSegformerForSemanticSegmentation.from_pretrained(
-            self.backbone,
-            num_labels=self.output_size,
-            id2label=id2label,
-            label2id=label2id,
-            ignore_mismatched_sizes=True,
-        )
+            The model.
 
-        return model
+        """
+        pass
