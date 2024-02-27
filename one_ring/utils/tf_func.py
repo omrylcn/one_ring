f175978b8f605f81f787f73e6f587ddbcf744f39
omrylcn <omeryalcin48@gmail.com>
2024-02-27 03:22:09 +0300

    Add set gpu memory

--- one_ring/utils/tf_func.py
+++ one_ring/utils/tf_func.py
@@ -3,3 +3,14 @@ import tensorflow as tf
 
 def is_tensor_or_variable(x):
     return tf.is_tensor(x) or isinstance(x, tf.Variable)
+
+
+def set_memory_growth():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+        except RuntimeError as e:
+            print(e)
+        
